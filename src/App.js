813eb4626ec2b4ca1f78b6dfcd6fdbfa0858e093
EC2 Default User <ec2-user@ip-172-31-94-114.ec2.internal>
2020-04-25 17:56:23 +0000

    add newgame component and connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import './AppCustom.css';
-import Game from './GameComponent/Game';
 import axios from 'axios';
+import Game from './GameComponent/Game';
+import NewGame from './NewGameComponent/NewGame';
 
 class App extends Component {
   state = {
-    games: []
+    games: [],
+    score: 0
   }
   
       componentDidMount() {
@@ -24,23 +26,49 @@ class App extends Component {
         axios({
           url: "https://react-notes-61c78.firebaseio.com/games.json",
           method: "post",
-          data: {title: "Dead Cells", 
-                img: "./media/dead-cells.jpg", 
-                description: `Dead Cells is a rogue-lite, metroidvania inspired, action-platformer. 
-                You'll explore a sprawling, ever-changing castle... 
-                assuming you're able to fight your way past its keepers in 2D souls-lite combat. No checkpoints. Kill, die, learn, repeat.`},
+          data: {title: "Firewatch", 
+                img: "./media/firewatch.jpg", 
+                description: `Firewatch is a mystery set in the Wyoming wilderness, 
+                where your only emotional lifeline is the person on the other end of a handheld radio. The year is 1989.`},
           contentType: "application/JSON"
         }).then(response => {
             console.log(response);
         }); 
       }
   
+        upVoteHandler = () => {
+          this.setState((prevState) => {
+            return {score: prevState.score + 1};
+          });
+        }
+      
+        downVoteHanlder = () => {
+          this.setState((prevState) => {
+            return {score : prevState.score -1};
+          });
+        }
+        
+        getNewData = () => {
+          axios.get("https://game-site-705ad.firebaseio.com/games.json")
+            .then(response => {
+                let newData = Object.values(response.data);
+                newData.map(el => {
+                  console.log(el.title + " " + el.description);
+                })
+            });
+        }
+
   render() {
     
-    
-    
-      let games = this.state.games.map(el => {
-          return <Game title={el.title} img={el.img} description={el.description} />
+      let games = this.state.games.map((el, ind) => {
+          return <Game 
+            title={el.title} 
+            img={el.img} 
+            description={el.description} 
+            upVote={this.upVoteHandler}
+            downVote={this.downVoteHanlder}
+            key={ind}
+            score={this.state.score}/>
       });
     
       return (
@@ -50,9 +78,10 @@ class App extends Component {
           </nav>
           <div className="container">
               {games}
+              <NewGame />
           </div>
           <div>
-            <button onClick={this.handleData}>Submit Data</button>
+            <button onClick={this.getNewData}>Get Data</button>
           </div>
         </div>
       );    
